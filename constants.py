5343386b21b381f0797b08a9d2aae080f3f62fbd
Marvil Garcia <marvigarcia015@gmail.com>
2022-03-29 20:29:34 -0400

    add a constants file.

---
+++ constants.py
@@ -0,0 +1,15 @@
+from game.shared.color import Color
+
+
+
+FRAME_RATE = 12
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
+ROWS = 40
+CAPTION = "Greed"
+#DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
+WHITE = Color(255, 255, 255)
+DEFAULT_ARTIFACTS = 10 # number of characters on the screen
